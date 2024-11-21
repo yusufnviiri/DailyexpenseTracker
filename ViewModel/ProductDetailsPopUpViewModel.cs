5cb35dfa221451aebeda8426d3d66716bd56ea3a
yusuf Nviiri <yusufnviiri@gmail.com>
2024-11-22 02:09:50 +0300

    modify popup

---
+++ ViewModel/ProductDetailsPopUpViewModel.cs
@@ -0,0 +1,21 @@
+﻿using attendance.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attendance.ViewModel
+{
+    public partial class ProductDetailsPopUpViewModel:SharedViewModel
+    {
+
+        public ProductDetailsPopUpViewModel(Product product)
+        {
+            Product = product;
+        }
+        [ObservableProperty]
+        Product product;
+    }
+}
--- Views/ProductDetailsPopup.xaml.cs
+++ Views/ProductDetailsPopup.xaml.cs
@@ -1,17 +1,17 @@
 using attendance.Models;
+using attendance.ViewModel;
 using CommunityToolkit.Maui.Views;
 
 namespace attendance.Views;
 
 public partial class ProductDetailsPopup : Popup
 {
-   public  Product Product { get; set; }
 
     public ProductDetailsPopup(Product product)
 	{
 		InitializeComponent();
-        BindingContext = this;
-       this.Product = product;
+        ProductDetailsPopUpViewModel viewModel = new ProductDetailsPopUpViewModel(product);
+        BindingContext = viewModel;
 	}
     private void closepopup(object sender, EventArgs e)
     {
